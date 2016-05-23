947d7cf084b1fea524d3fc55bef51d77a3d25432
keithsjohnson <keithsjohnson@yahoo.com>
2016-05-23 09:04:50 +0100

    Add Lambda Node4.3 runtime and Fix API Gateway Errors with depends on to revent 2 runs.

--- terraform/apigateway/apigateway.tf
+++ terraform/apigateway/apigateway.tf
@@ -31,6 +31,7 @@ resource "aws_api_gateway_resource" "FeedbackAPI3" {
 }
 
 resource "aws_api_gateway_method" "FeedbackAPI3" {
+  depends_on = ["aws_api_gateway_model.FeedbackModel3"]
   rest_api_id = "${aws_api_gateway_rest_api.FeedbackAPI3.id}"
   resource_id = "${aws_api_gateway_resource.FeedbackAPI3.id}"
   request_models = {"application/json" = "FeedbackModel3"}
@@ -56,6 +57,7 @@ resource "aws_api_gateway_method_response" "FeedbackAPI3MethodResponse" {
 }
 
 resource "aws_api_gateway_integration_response" "FeedbackAPI3IntegrationResponse" {
+  depends_on = ["aws_api_gateway_integration.FeedbackAPI3"]
   rest_api_id = "${aws_api_gateway_rest_api.FeedbackAPI3.id}"
   resource_id = "${aws_api_gateway_resource.FeedbackAPI3.id}"
   http_method = "${aws_api_gateway_method.FeedbackAPI3.http_method}"
@@ -64,12 +66,14 @@ resource "aws_api_gateway_integration_response" "FeedbackAPI3IntegrationResponse
 
 # GET to FeedbackAggregatesScanAllLambda3 Lambda
 resource "aws_api_gateway_resource" "FeedbackTotals" {
+  depends_on = ["aws_api_gateway_rest_api.FeedbackAPI3"]
   rest_api_id = "${aws_api_gateway_rest_api.FeedbackAPI3.id}"
   parent_id = "${aws_api_gateway_rest_api.FeedbackAPI3.root_resource_id}"
   path_part = "FeedbackTotals"
 }
 
 resource "aws_api_gateway_method" "FeedbackAggregatesScanAllLambda3" {
+  depends_on = ["aws_api_gateway_resource.FeedbackTotals"]
   rest_api_id = "${aws_api_gateway_rest_api.FeedbackAPI3.id}"
   resource_id = "${aws_api_gateway_resource.FeedbackTotals.id}"
   http_method = "GET"
@@ -77,6 +81,7 @@ resource "aws_api_gateway_method" "FeedbackAggregatesScanAllLambda3" {
 }
 
 resource "aws_api_gateway_integration" "FeedbackAggregatesScanAllLambda3" {
+  depends_on = ["aws_api_gateway_method.FeedbackAggregatesScanAllLambda3"]
   rest_api_id = "${aws_api_gateway_rest_api.FeedbackAPI3.id}"
   resource_id = "${aws_api_gateway_resource.FeedbackTotals.id}"
   http_method = "${aws_api_gateway_method.FeedbackAggregatesScanAllLambda3.http_method}"
@@ -87,6 +92,7 @@ resource "aws_api_gateway_integration" "FeedbackAggregatesScanAllLambda3" {
 }
 
 resource "aws_api_gateway_method_response" "FeedbackAggregatesScanAllLambda3MethodResponse" {
+  depends_on = ["aws_api_gateway_method.FeedbackAggregatesScanAllLambda3"]
   rest_api_id = "${aws_api_gateway_rest_api.FeedbackAPI3.id}"
   resource_id = "${aws_api_gateway_resource.FeedbackTotals.id}"
   http_method = "${aws_api_gateway_method.FeedbackAggregatesScanAllLambda3.http_method}"
@@ -94,6 +100,7 @@ resource "aws_api_gateway_method_response" "FeedbackAggregatesScanAllLambda3Meth
 }
 
 resource "aws_api_gateway_integration_response" "FeedbackAggregatesScanAllLambda3IntegrationResponse" {
+  depends_on = ["aws_api_gateway_integration.FeedbackAggregatesScanAllLambda3"]
   rest_api_id = "${aws_api_gateway_rest_api.FeedbackAPI3.id}"
   resource_id = "${aws_api_gateway_resource.FeedbackTotals.id}"
   http_method = "${aws_api_gateway_method.FeedbackAggregatesScanAllLambda3.http_method}"
--- terraform/lambda/variables.tf
+++ terraform/lambda/variables.tf
@@ -1,3 +1,4 @@
 variable "lambda_role_arn" {}
 variable "aggregates_table_arn" {}
 variable "aggregates_table_stream_arn" {}
+variable "lambda_node_runtime" { default = "nodejs4.3" }
